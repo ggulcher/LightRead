6a5f5cd1589f2519ba252cfc39cd65d63bc94ca9
SunsetSuperman <SunsetSuperman@users.noreply.github.com>
2020-07-19 01:09:34 -0400

    Completed user speed selection

--- LightRead/src/LightReadMain.java
+++ LightRead/src/LightReadMain.java
@@ -1,11 +1,22 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LightReadMain {
 	
 	// Save for custom file destination
 	// String file_path;
+	static Map<Integer, Integer> speed_map = new HashMap<>() {
+		{
+			put(200, 265);
+			put(300, 165);
+			put(500, 75);
+			put(800, 28);
+		}
+	};
+	
 	static int word_speed;
 	
 	public static void main(String args[]) throws FileNotFoundException, InterruptedException {
@@ -13,7 +24,11 @@ public class LightReadMain {
 		LightReadMain overlord = new LightReadMain();
 		Scanner decide_speed = new Scanner(System.in);
 		System.out.println("How quickly would you like to read?\n{200,300,500,800}");
-		overlord.set_speed(decide_speed.nextInt());
+		int wanted_speed = decide_speed.nextInt();
+		
+		if(speed_map.containsKey(wanted_speed)) {
+			word_speed = speed_map.get(wanted_speed);
+		}
 		
 		File file = new File("C:\\Users\\Griff_Home_PC\\Desktop\\Java_Projects\\LightRead\\Sage.txt");
 		Scanner scan = new Scanner(file);
